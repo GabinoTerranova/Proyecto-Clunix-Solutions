71d8d4f92b8f01e7d3c56f5c62c829fbdd513f88
GabinoTerranova <146039885+GabinoTerranova@users.noreply.github.com>
2024-07-07 18:35:59 -0300

    Front con Node / Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./Front/**/*.html', './Front/**/*.js'], // Actualizado de purge a content
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
